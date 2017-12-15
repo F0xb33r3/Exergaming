0720a0c72f8a5331fad62b22131d945cefbd6312
m3rd833r3 <MBayer1@gmx.at>
2017-12-15 21:39:16 +0100

    Correct Order of Code. TODO: Test

--- UserController/Roll a Ball/Assets/Skript/PlayerController.cs
+++ UserController/Roll a Ball/Assets/Skript/PlayerController.cs
@@ -12,34 +12,80 @@ public class PlayerController : MonoBehaviour {
 	private float fStartTime;
 
     private Rigidbody rb;
+
 	private float moveHorizontal, moveHigh, moveVertical;
+	private float fCurrentPos;
+	private float fPlanedPos;
+
+	private float fStartPos;
+	private bool bJumpUp;
+
+	private enum Direction {None, Up, Right, Down, Left};
+	int CurrentDirection;
 
     void Start () //run before randering a frame
     {
         rb = GetComponent<Rigidbody>();
+		CurrentDirection = (int)Direction.None;
 		if (speed > fSizeOfOnePath && speed > fJumpHigh) {
 			//TODO Value Error
 		}
     }
 
     void FixedUpdate () //run before perform phisical cacluations
-	{		
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			Jump ();
-			return;
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			Duck();
-			return;
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			GoRight ();
-			return;
-		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			GoLeft();
-			return;
-		} else {/*
-			Vector3 movement = new Vector3 (speed, 0.0f, 0.0f);
-			rb.AddForce (movement);*/
+	{	
+		//setting of aim
+		if (Input.GetKey (KeyCode.UpArrow) && CurrentDirection == (int)Direction.None) {// jump +y axis
+			moveHorizontal = moveVertical = 0.0f;
+			fCurrentPos =  rb.position.z;
+			fPlanedPos = fCurrentPos + fJumpHigh;
+			fStartPos = rb.position.z;
+			bJumpUp = true;
+			fStartTime = Time.fixedTime;
+			CurrentDirection = (int)Direction.Up;
+
+		} else if (Input.GetKeyDown (KeyCode.DownArrow) && CurrentDirection == (int)Direction.None) {
+			//TODO
+			CurrentDirection = (int)Direction.Down;
+
+		} else if (Input.GetKey (KeyCode.RightArrow) && CurrentDirection == (int)Direction.None) {// move right +z axis
+			moveHigh = moveVertical = 0.0f;
+			fCurrentPos =  rb.position.z;
+			fPlanedPos = fCurrentPos + fSizeOfOnePath;
+			fStartTime = Time.fixedTime;
+			CurrentDirection = (int)Direction.Right;
+
+		} else if (Input.GetKey (KeyCode.LeftArrow) && CurrentDirection == (int)Direction.None) { // move left -z axis (only horizontal)
+			moveHigh = moveVertical = 0.0f;
+			fCurrentPos =  rb.position.z;
+			fPlanedPos = fCurrentPos - fSizeOfOnePath;
+			fStartTime = Time.fixedTime;
+			CurrentDirection = (int)Direction.Left;
+		} 
+
+		//check Timeout
+		if(fCurrentTime-fStartTime > fTimeOut && CurrentDirection != (int)Direction.None){
+			//TODO error
+			return;	
 		}
+
+		//running
+		if (Input.GetKey (KeyCode.UpArrow) && CurrentDirection == (int)Direction.Up) {
+			fCurrentTime = Time.fixedTime;
+			Jump ();
+		} else if (Input.GetKey (KeyCode.DownArrow) && CurrentDirection == (int)Direction.Down) {
+			fCurrentTime = Time.fixedTime;
+			Duck();
+		} else if (Input.GetKey (KeyCode.RightArrow) && CurrentDirection == (int)Direction.Right) {
+			fCurrentTime = Time.fixedTime;
+			GoRight ();
+		} else if (Input.GetKey (KeyCode.LeftArrow) && CurrentDirection == (int)Direction.Left) {
+			fCurrentTime = Time.fixedTime;
+			GoLeft ();
+		} else if (CurrentDirection != (int)Direction.None){
+			//balanced lost -> do error or give time to get balance back TODO
+		}
+		return;
 	}
 
 	void CheckBalance(){//TODO
@@ -50,125 +96,71 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void GoLeft(){ // move left -z axis (only horizontal)
-		moveHigh = moveVertical = 0.0f;
-		float fCurrentPos =  rb.position.z;
-		float fPlanedPos = fCurrentPos - fSizeOfOnePath;
+		//core function
+		fCurrentPos = rb.position.z;
+		if (fCurrentPos > fPlanedPos) { //going left has to get smaller
+			//get balance values for adaptive settings  TODO
 
-		fStartTime = Time.fixedTime;
-
-		while (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			//check Timeout
-			fCurrentTime = Time.fixedTime;
-			if(fCurrentTime-fStartTime > fTimeOut){
-				//TODO error
-				return;	
-			}
-
-			//core function
-			fCurrentPos = rb.position.z;
-			if (fCurrentPos > fPlanedPos) { //going left has to get smaller
-				//get balance values for adaptive settings  TODO
-
-				//update position
-				moveHorizontal = Input.GetAxis("Horizontal");
-				Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-				rb.AddForce (movement*speed);
-			} else {
-				CheckBalance();
-				return;
-			}
+			//update position
+			moveHorizontal = Input.GetAxis("Horizontal");
+			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
+			rb.AddForce (movement*speed);
+		} else {
+			CurrentDirection = (int)Direction.None;
+			CheckBalance();
 		}
-		if(fCurrentPos > fSizeOfOnePath){
-			//balanced lost -> do error or give time to get balance back TODO
-		}
+		return;
 	}
 
 	void GoRight(){ // move right +z axis
-		moveHigh = moveVertical = 0.0f;
-		float fCurrentPos =  rb.position.z;
-		float fPlanedPos = fCurrentPos + fSizeOfOnePath;
+		//core function
+		fCurrentPos = rb.position.z; 
+		if (fCurrentPos < fPlanedPos) { //going left has to get bigger
+			//get balance values for adaptive settings afterwards TODO
 
-		fStartTime = Time.fixedTime;
-
-		while (Input.GetKeyDown (KeyCode.RightArrow)) {
-			//check Timeout
-			fCurrentTime = Time.fixedTime;
-			if(fCurrentTime-fStartTime > fTimeOut){
-				//TODO error
-				return;	
-			}
-
-			//core function
-			fCurrentPos = rb.position.z; 
-			if (fCurrentPos < fPlanedPos) { //going left has to get bigger
-				//get balance values for adaptive settings afterwards TODO
-
-				//update position
-				moveHorizontal = Input.GetAxis("Horizontal");
-				Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-				rb.AddForce (movement*speed);
-			} else {
-				CheckBalance();
-				return;
-			}
+			//update position
+			moveHorizontal = Input.GetAxis("Horizontal");
+			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
+			rb.AddForce (movement*speed);
+		} else {
+			CurrentDirection = (int)Direction.None;
+			CheckBalance();
 		}
-		if(fCurrentPos < fSizeOfOnePath){
-			//balanced lost -> do error or give time to get balance back TODO
-		}		
+		return;
 	}
 
 	void Jump(){ // jump +y axis
-		moveHorizontal = moveVertical = 0.0f;
+		//core function
+		fCurrentPos = rb.position.z; 
 
-		float fCurrentPos =  rb.position.z;
-		float fPlanedPos = fCurrentPos + fJumpHigh;
-		float fStartPos = rb.position.z;
-		bool bJumpUp = true;
+		//jump up
+		if (fCurrentPos < fPlanedPos && bJumpUp) { 
+			//get balance values for adaptive settings afterwards TODO
 
-		fStartTime = Time.fixedTime;
+			//update position
+			moveHigh = Input.GetAxis ("Vertical");
+			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
+			rb.AddForce (movement * speed);
+		} else {
+			bJumpUp = false;
+		} 
 
-		while (Input.GetKeyDown (KeyCode.UpArrow)) {
-			//check Timeout
-			fCurrentTime = Time.fixedTime;
-			if(fCurrentTime-fStartTime > fTimeOut){
-				//TODO error
-				return;	
-			}
+		//fall down
+		if (fCurrentPos > fStartPos && !bJumpUp) {
+			//get balance values for adaptive settings afterwards TODO
 
-			//core function
-			fCurrentPos = rb.position.z; 
-
-			//jump up
-			if (fCurrentPos < fPlanedPos && bJumpUp) { 
-				//get balance values for adaptive settings afterwards TODO
-
-				//update position
-				moveHigh = Input.GetAxis ("Vertical");
-				Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-				rb.AddForce (movement * speed);
-			} else {
-				bJumpUp = false;
-			} 
-
-			//fall down
-			if (fCurrentPos > fStartPos && !bJumpUp) {
-				//get balance values for adaptive settings afterwards TODO
-
-				//update position
-				moveHigh = Input.GetAxis("Vertical")*(-1);
-				Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-				rb.AddForce (movement*speed);
-			} else {
-				CheckBalance();
-				return;
-			}
-		}
-		if(fCurrentPos < fSizeOfOnePath){
-			//balanced lost -> do error or give time to get balance back TODO
+			//update position
+			moveHigh = Input.GetAxis("Vertical")*(-1);
+			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
+			rb.AddForce (movement*speed);
+		} else {
+			CurrentDirection = (int)Direction.None;
+			CheckBalance();				
 		}
 	}
 
 	void Duck () { // lean back do not anything solely change animation??
 		//TODO
+		CurrentDirection = (int)Direction.None;
 	}   
 }
