8e1e2ebe86347124cebcb074096a611f97970b10
m3rd833r3 <MBayer1@gmx.at>
2017-12-17 12:46:24 +0100

    Tested and working Prototype

--- UserController/Roll a Ball/Assets/Skript/PlayerController.cs
+++ UserController/Roll a Ball/Assets/Skript/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
@@ -20,6 +21,8 @@ public class PlayerController : MonoBehaviour {
 	private float fStartPos;
 	private bool bJumpUp;
 
+	private float fAproximatly;
+
 	private enum Direction {None, LeaningLeft, LeaningRight, Jumping, JumpingOnLeftLeg, JumpingOnRightLeg, BalancingLeftLeg, BalancingRightLeg};
 	int CurrentDirection;
 
@@ -30,6 +33,7 @@ public class PlayerController : MonoBehaviour {
 		if (speed > fSizeOfOnePath && speed > fJumpHigh) {
 			//TODO Value Error
 		}
+		fAproximatly = Mathf.Pow(10,-5); 
     }
 
     void FixedUpdate () //run before perform phisical cacluations
@@ -54,65 +58,64 @@ public class PlayerController : MonoBehaviour {
 			CurrentDirection = (int)Direction.JumpingOnLeftLeg;
 		} else if (Input.GetKey (KeyCode.UpArrow) && CurrentDirection == (int)Direction.None) {// jump +y axis
 			moveHorizontal = moveVertical = 0.0f;
-			fCurrentPos =  rb.position.z;
+			fCurrentPos =  rb.position.y;
 			fPlanedPos = fCurrentPos + fJumpHigh;
-			fStartPos = rb.position.z;
+			fStartPos = rb.position.y;
 			bJumpUp = true;
-			fStartTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			CurrentDirection = (int)Direction.Jumping;
 		} else
 		
 		// Leaning
 		if (Input.GetKey (KeyCode.RightArrow) && CurrentDirection == (int)Direction.None) {// move right +z axis
 			moveHigh = moveVertical = 0.0f;
-			fCurrentPos =  rb.position.z;
+			fCurrentPos =  rb.position.x;
 			fPlanedPos = fCurrentPos + fSizeOfOnePath;
-			fStartTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			CurrentDirection = (int)Direction.LeaningRight;
 
 		} else if (Input.GetKey (KeyCode.LeftArrow) && CurrentDirection == (int)Direction.None) { // move left -z axis (only horizontal)
 			moveHigh = moveVertical = 0.0f;
-			fCurrentPos =  rb.position.z;
+			fCurrentPos =  rb.position.x;
 			fPlanedPos = fCurrentPos - fSizeOfOnePath;
-			fStartTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			CurrentDirection = (int)Direction.LeaningLeft;
 		} 
-			
-
+					
 		//check Timeout
+		fCurrentTime = Time.realtimeSinceStartup;
 		if(fCurrentTime-fStartTime > fTimeOut && CurrentDirection != (int)Direction.None){
 			//TODO error
 			return;	
 		}
-
-
+			
 		// Balancing on One Leg
 		if (Input.GetKey (KeyCode.RightArrow) && Input.GetKey (KeyCode.DownArrow) && CurrentDirection == (int)Direction.BalancingRightLeg) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			//TODO Function
 		} else if (Input.GetKey (KeyCode.LeftArrow) && Input.GetKey (KeyCode.DownArrow) && CurrentDirection == (int)Direction.BalancingLeftLeg) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			//TODO Function
 		} else
 			
 		//Jumping 
 		if (Input.GetKey (KeyCode.RightArrow) && Input.GetKey (KeyCode.UpArrow) && CurrentDirection == (int)Direction.JumpingOnRightLeg) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			//TODO Function
 		} else if (Input.GetKey (KeyCode.LeftArrow) && Input.GetKey (KeyCode.UpArrow) && CurrentDirection == (int)Direction.JumpingOnLeftLeg) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			//TODO Function
 		} else if (Input.GetKey (KeyCode.UpArrow) && CurrentDirection == (int)Direction.Jumping) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			Jump ();
 		} else 		
 				
 		// Leaning
 		if (Input.GetKey (KeyCode.RightArrow) && CurrentDirection == (int)Direction.LeaningRight) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			LeanRight ();
 		} else if (Input.GetKey (KeyCode.LeftArrow) && CurrentDirection == (int)Direction.LeaningLeft) {
-			fCurrentTime = Time.fixedTime;
+			fStartTime = Time.realtimeSinceStartup;
 			LeanLeft ();
 		} else 
 					
@@ -132,15 +135,23 @@ public class PlayerController : MonoBehaviour {
 
 	void LeanLeft(){ // move left -z axis (only horizontal)
 		//core function
-		fCurrentPos = rb.position.z;
+		fCurrentPos = rb.position.x;
 		if (fCurrentPos > fPlanedPos) { //going left has to get smaller
 			//get balance values for adaptive settings  TODO
 
 			//update position
 			moveHorizontal = Input.GetAxis("Horizontal");
 			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-			rb.transform.position += movement*speed;
+			movement *= speed;
+
+			if (Val1SmallerOrAlmostEqual(fCurrentPos + movement.x, fPlanedPos)) {				//do not overstep
+				movement.Set(fPlanedPos, rb.position.y, rb.position.z);
+				rb.transform.position = movement;					
+			} else {
+				rb.transform.position += movement;
+			}
 		} else {
+			//TODO?? display somethink that this task is done???
 			CurrentDirection = (int)Direction.None;
 			CheckBalance();
 		}
@@ -149,7 +160,7 @@ public class PlayerController : MonoBehaviour {
 
 	void LeanRight(){ // move right +z axis
 		//core function
-		fCurrentPos = rb.position.z; 
+		fCurrentPos = rb.position.x; 
 
 		if (fCurrentPos < fPlanedPos) { //going left has to get bigger
 			//get balance values for adaptive settings afterwards TODO
@@ -157,8 +168,16 @@ public class PlayerController : MonoBehaviour {
 			//update position
 			moveHorizontal = Input.GetAxis("Horizontal");
 			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-			rb.transform.position += movement*speed;
+			movement *= speed;
+
+			if (Val1BigerOrAlmostEqual(fCurrentPos + movement.x, fPlanedPos)) {			//do not overstep
+				movement.Set(fPlanedPos, rb.position.y, rb.position.z);
+				rb.transform.position = movement;
+			} else {
+				rb.transform.position += movement;
+			}
 		} else {
+			//TODO?? display something that this task is done???
 			CurrentDirection = (int)Direction.None;
 			CheckBalance();
 		}
@@ -167,7 +186,7 @@ public class PlayerController : MonoBehaviour {
 
 	void Jump(){ // jump +y axis
 		//core function
-		fCurrentPos = rb.position.z; 
+		fCurrentPos = rb.position.y; 
 
 		//jump up
 		if (fCurrentPos < fPlanedPos && bJumpUp) { 
@@ -176,8 +195,15 @@ public class PlayerController : MonoBehaviour {
 			//update position
 			moveHigh = Input.GetAxis ("Vertical");
 			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-			rb.transform.position += movement*speed;
-		} else {
+			movement *= speed;
+
+			if (Val1BigerOrAlmostEqual(fCurrentPos + movement.y, fPlanedPos)) {			//do not overstep
+				movement.Set(rb.position.x, fPlanedPos, rb.position.z);
+				rb.transform.position = movement;
+			} else {
+				rb.transform.position += movement;
+			}
+		} else if(bJumpUp){
 			bJumpUp = false;
 		} 
 
@@ -188,10 +214,36 @@ public class PlayerController : MonoBehaviour {
 			//update position
 			moveHigh = Input.GetAxis("Vertical")*(-1);
 			Vector3 movement = new Vector3 (moveHorizontal, moveHigh, moveVertical);
-			rb.transform.position += movement*speed;
-		} else {
+			movement *= speed;
+
+			if (Val1SmallerOrAlmostEqual(fCurrentPos + movement.y, fStartPos)) {		//do not overstep
+				movement.Set(rb.position.x, fStartPos, rb.position.z);
+				rb.transform.position = movement;
+			} else {
+				rb.transform.position += movement;
+			}
+		} else if(!bJumpUp) {
+			//TODO?? display somethink that this task is done???
+
 			CurrentDirection = (int)Direction.None;
 			CheckBalance();				
 		}
 	}  
+		
+	bool AlmostEqual(float Val1, float Val2){
+		if (Mathf.Abs (Val1 - Val2) <= fAproximatly) return true;
+		return false;
+	}
+
+	bool Val1BigerOrAlmostEqual(float Val1, float Val2){
+		if(Val1 > Val2 || AlmostEqual(Val1,Val2)) return true;
+		return false;
+	}
+
+	bool Val1SmallerOrAlmostEqual(float Val1, float Val2){
+		if(Val1 < Val2 || AlmostEqual(Val1,Val2)) return true;
+		return false;
+	}
 }
+
+
