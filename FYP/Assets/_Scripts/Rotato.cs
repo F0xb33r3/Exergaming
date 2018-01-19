60fd947f91e9e6dd5fa2fa80c3945f422351e785
Connor Maguire <C00189537@itcarlow.ie>
2018-01-19 16:27:44 +0100

    score and timer

---
+++ FYP/Assets/_Scripts/Rotato.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotato : MonoBehaviour {
+
+    public Vector3 rotationVector;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("Rotate");
+        transform.Rotate(rotationVector * Time.deltaTime);
+	}
+}
