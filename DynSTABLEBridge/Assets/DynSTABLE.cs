9db876e93da7550a32c6150651df66bc3d7120c4
Moritz Dittgen <moritz.dittgen@me.com>
2018-01-19 11:19:22 +0100

    Added DynSTABLE Module

---
+++ DynSTABLEBridge/Assets/DynSTABLE.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynSTABLE : MonoBehaviour {
+
+	public float a = 0.0f;
+	public float b = 0.0f;
+
+	public float c = 1.0f;
+
+	void NewDFlowData(DFlowNetwork network) {
+		a = network.getOutput(0);
+		b = network.getOutput(1);
+
+		Debug.Log (a + " " + b);
+
+		network.setInput (0, c);
+	}
+		
+	// Use this for initialization
+	void Start () {
+		DFlowNetwork.OnNewData += NewDFlowData;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
